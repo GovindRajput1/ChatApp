dbe8463d7f38f3cdd5cd338200f60a39df3bc63e
Govind <govindkumarrajput1@gmail.com>
2024-02-29 23:17:00 +0530

    spelling error

--- ChatApp/ChatApp/settings.py
+++ ChatApp/ChatApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@efho+(@ki#8er)e+r_s*d08o!0e8ifda&1u4_mcehytp7rpbu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh',]
 
 # Application definition
 
